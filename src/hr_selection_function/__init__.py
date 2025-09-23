79a9bf2955d33764d1b3d43fe3656adbde1b0b26
emilyhunt <emily.hunt.physics@gmail.com>
2025-09-23 15:41:11 +0200

    Bugfix to circular import on package start

--- src/hr_selection_function/__init__.py
+++ src/hr_selection_function/__init__.py
@@ -1,3 +1,7 @@
+from .config import _DEFAULT_DIRECTORY
 from .data import set_data_directory
 from .models import HR24SelectionFunction, NStarsPredictor
 from .gaia import M10Estimator, MSubsampleEstimator, GaiaDensityEstimator
+
+
+set_data_directory(_DEFAULT_DIRECTORY)
--- src/hr_selection_function/config.py
+++ src/hr_selection_function/config.py
@@ -3,17 +3,14 @@
 import os
 from pathlib import Path
 
-from hr_selection_function.data import set_data_directory
-
 
 # Internal config storage dict (user access not supported!)
 _CONFIG = dict()
 
 # Setup default directory
-directory = os.getenv("HRSF_DATA", None)
-if directory is None:
-    directory = Path.home() / ".hr_selection_function"
-set_data_directory(directory)
+_DEFAULT_DIRECTORY = os.getenv("HRSF_DATA", None)
+if _DEFAULT_DIRECTORY is None:
+    _DEFAULT_DIRECTORY = Path.home() / ".hr_selection_function"
 
 # Some hard defaults
 _CONFIG["data_url"] = "https://example.com"  # Todo
